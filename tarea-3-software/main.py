aa3181fdf964f2882f95d44d149ec69234013ee5
Carlos <cserradag96@gmail.com>
2017-02-02 16:03:50 -0400

    Agregada la clase Registro para llevar control de la billetera

--- tarea-3-software/main.py
+++ tarea-3-software/main.py
@@ -1,8 +1,16 @@
+# -*- encoding: utf-8 -*-
 '''
-Created on Feb 2, 2017
+Created on 1 feb. 2017
 
-@author: pablodbc
+@author: carlos
 '''
+from datetime import *
 
-if __name__ == '__main__':
-    pass
+class Registro:
+    def __init__(self, monto, fecha, localID):
+        self.monto = monto
+        self.fecha = datetime.strftime(fecha,'%d/%m/%Y %H:%M')
+        self.localID = localID
+
+    def __str__(self):
+        return "\nMonto: "+str(self.monto)+"\nFecha: "+self.fecha+"\nEstablecimiento: "+str(self.localID)+"\n"
