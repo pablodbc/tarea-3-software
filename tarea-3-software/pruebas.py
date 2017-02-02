1efbea7b5fcad75abe7dbda20d600bd5468d09e2
Carlos <cserradag96@gmail.com>
2017-02-02 16:28:46 -0400

    Agregados nuevos casos de prueba. Resultados esperados: 3 fallos, 1 OK. Resultados obtenidos: 3 fallos, 1 OK

--- tarea-3-software/pruebas.py
+++ tarea-3-software/pruebas.py
@@ -14,5 +14,17 @@ class Test(unittest.TestCase):
     def testCedulaNegativa(self):
         billetera = BilleteraElectronica(0,"Pablo","Betancourt",-1,1234)
 
+    def testRecargaNoValida(self):
+            billetera = BilleteraElectronica(0,"Pablo","Betancourt",1,1234)
+            billetera.recargar(-1000,"Pablo")
+    
+    def testDebitoConBalanceMenor(self):
+        billetera = BilleteraElectronica(0,"Pablo","Betancourt",1,1234)
+        billetera.consumir(1234,1,"Pablo")
+
+    def testDebitoConBalanceMinimamenteMayor(self):
+            billetera = BilleteraElectronica(0,"Pablo","Betancourt",1,1234,1.000001)
+            billetera.consumir(1234,1,"Pablo")
+
 if __name__ == "__main__":
     unittest.main()
